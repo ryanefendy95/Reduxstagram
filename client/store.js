0ffa7498d61d3684bb62c15df0a77c759ace939f
Ryan Efendy <ryanefendy95@gmail.com>
2017-07-17 09:43:24 -0700

    progress..

--- client/store.js
+++ client/store.js
@@ -1,3 +1,16 @@
-/**
- * Created by ryanefendy95 on 7/15/17.
- */
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+// import promise from 'redux-promise-middleware';
+// import userReducer from './reducers/user';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+
+export default createStore(
+    userReducer,
+    applyMiddleware(
+        logger(),
+        promise()
+    )
+);
