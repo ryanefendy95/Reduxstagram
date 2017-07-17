352801617995562a8ea795c457742f02852f89b1
Ryan Efendy <ryanefendy95@gmail.com>
2017-07-17 07:20:05 -0700

    redux #4

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,18 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import css from './styles/style.styl';
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route component={Main}/>
+            <Route exact path="/" component={PhotoGrid} />
+            <Route path="/view/:postId" component={Single} />
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
